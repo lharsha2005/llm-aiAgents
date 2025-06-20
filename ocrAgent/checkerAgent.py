2322f7545d7c07992117793d2f811cd19af50850
Lebaka Harsha Vardhan <lharsha2005@gmail.com>
2025-06-20 11:50:31 +0530

    Update checkerAgent.py

--- ocrAgent/checkerAgent.py
+++ ocrAgent/checkerAgent.py
@@ -21,27 +21,44 @@ class Checker(BaseModel):
                     "'END' if given conditions do not match, "
     )
 
-def run_checker(text: str):
+    flag: Literal[True, False]=Field(
+        description="Specifies if the conditions are matched:"
+                    "'True' if conditions match,"
+                    "'False' if conditions do not match"
+    )
+
+    reson: str=Field(
+        description="The reason for the decision, providing context on why a particular worker was chosen."
+    )
+
+def run_checker(text: str, question: str):
     prompt=PromptTemplate.from_template(
         """
         text:{text}
+        question:{question}
         geivn text form ocr decide weather the given conditions satisfy.
         if the conditions satisfy goto supervisor next else end.
         conditions:
-            1.if text is relavent to bank statements
-            2.if final balance is less than 10000000
+            1.if text is relavent to bank statements.
+            2.if final balance in text is less than 10000000.
+            3.if question is strictly to realvent to bank statements transations and can only be solved using given transations.
+        
+        **NOTE**
+            -all codition should satisfiy, even if one condtion is not stisfied do not go to supervisor.
         """
     )
 
-    res=llm.with_structured_output(Checker).invoke(prompt.format(text=text))
+    res=llm.with_structured_output(Checker).invoke(prompt.format(text=text, question=question))
 
     goto=res.next
+    flag=res.flag
 
     if goto==END or goto=="END":
         goto==END
 
+    print("#"*80)
+    print("checker reson:",res.reson)
+    print("#"*80)
+    print()
     print("cheker->",goto)
-    return Command(
-        update={"check": True},
-        goto=goto
-    )
+    return [goto,flag]
