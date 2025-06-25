8de4f93c2ee8b38e24215604d52215411b4172e4
Lebaka Harsha Vardhan <lharsha2005@gmail.com>
2025-06-25 14:23:35 +0530

    Update graph.py

--- bankChatbot/graph.py
+++ bankChatbot/graph.py
@@ -5,9 +5,11 @@ from State import State
 from ocrAgent import run_ocr
 from supervisor import supervisor
 from queryAgent import run_query
+from VisionAgent import vision
 
 workflow=StateGraph(State)
-workflow.add_node("ocr",run_ocr)
+workflow.add_node("ocr",run_ocr)#using paddleocr
+workflow.add_node("ocr",vision)#using llm
 workflow.add_node("supervisor",supervisor)
 workflow.add_node("query",run_query)
 
