ecba177041075755a083cdde8227561abbb93fc7
Lebaka Harsha Vardhan <lharsha2005@gmail.com>
2025-06-25 11:54:48 +0530

    Add files via upload

---
+++ bankChatbot/graph.py
@@ -0,0 +1,29 @@
+from langgraph.graph import StateGraph,START,END
+from State import State
+
+
+from ocrAgent import run_ocr
+from supervisor import supervisor
+from queryAgent import run_query
+
+workflow=StateGraph(State)
+workflow.add_node("ocr",run_ocr)
+workflow.add_node("supervisor",supervisor)
+workflow.add_node("query",run_query)
+
+workflow.add_edge(START,"supervisor")
+workflow.add_edge("query",END)
+
+graph=workflow.compile()
+
+# msg = {
+#     "question": "What documents are required to open an account?",
+#     "img_path": None,
+#     "ocr_text": None,
+#     "answer": None
+# }
+
+
+# res=graph.invoke(msg)
+# print()
+# print("Final Answer:", res["answer"])
